768e7eb6f23331345bd03668df43bcfc968f831c
powpulous <hilmanamirulhaq@mail.ugm.ac.id>
2024-09-08 20:55:15 +0700

    Add files via upload

---
+++ source code 2.py
@@ -0,0 +1,6 @@
+m =['Alice', 'ants','Bob','badgers','Carol','cats']
+m.sort(key=str.lower)
+print(m)
+spam =['a','z','A','Z']
+spam.sort(key=str.lower)
+print(spam)
---
+++ source code 7.py
@@ -0,0 +1,24 @@
+z = [1, 3, 2, 4]
+z.sort()
+print(z)
+
+y = ['Alice', 'Bob']
+y.sort()
+print(y)
+print("Hello there!\nHow are you?\nI\'m doing fine.")
+
+multi_line = """Hello there!
+How are you?
+I'm fine."""
+print(multi_line)
+
+spam = ' Hello World '
+print(spam.strip())
+print(spam.lstrip())
+print(spam.rstrip())
+
+print(','.join(['cats', 'rats', 'bats']))
+print(' '.join(['My', 'name', 'is', 'Simon']))
+print('ABC'.join(['My', 'name', 'is', 'Simon']))
+print('My name is Simon'.split())
+print('MyABCnameABCisABCSimon'.split('ABC'))
