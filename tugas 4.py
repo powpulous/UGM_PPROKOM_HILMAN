2e84f038ce8fdd554dc4f0469d8b2093603ae1c5
powpulous <hilmanamirulhaq@mail.ugm.ac.id>
2024-10-06 20:21:59 +0700

    Add files via upload

---
+++ tugas 4.py
@@ -0,0 +1,10 @@
+dict = {
+    'Name': 'Zara',
+    'Age': 7, 
+    'Class': 'First'
+}
+dict['Age'] = 8
+dict['School'] = "DPS School"
+print(dict)
+print ("dict['Age']: ", dict['Age'])
+print ("dict['School']: ", dict['School'])
---
+++ tugas 6.py
@@ -0,0 +1,10 @@
+dict = {
+    'name':'zara',
+    'age':7,
+    'class':'first'
+}
+del dict['name']
+dict.clear()
+del dict
+print('dict[age] :', dict['age'])
+print('dict[school] :', dict['school'])
